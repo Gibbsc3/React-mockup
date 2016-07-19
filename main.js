b96fcb3dfe3bc1dbd664abbcac3b01db41abbd34
Tony <gibbsc@indiana.edu>
2016-07-19 16:29:58 -0400

    first

--- main.js
+++ main.js
@@ -9,4 +9,7 @@ ReactDOM.render(
   <div>
   <Post postLink="google.com" header="Terrible Style" date="10/19/2004" link="https://pbs.twimg.com/profile_images/24679042/317x317_soulja_boy_400x400.jpg" />
   <Post postLink="google.com" header="Childhood Poster" date="10/19/1990" link="http://www.sellit.co.nz/au/store/media/homieportrait.jpg" />
+  <Post postLink="google.com" header="Childhood Poster" date="10/19/1990" link="http://www.sellit.co.nz/au/store/media/homieportrait.jpg" />
+  <Post postLink="google.com" header="Childhood Poster" date="10/19/1990" link="http://www.sellit.co.nz/au/store/media/homieportrait.jpg" />
+  <Post postLink="google.com" header="Childhood Poster" date="10/19/1990" link="http://www.sellit.co.nz/au/store/media/homieportrait.jpg" />
 </div>, document.getElementById('app'))
